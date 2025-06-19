a16abf3e31d71678de77569dd1860856ef6f94d4
reuben148 <reubenluka555@gmail.com>
2025-06-19 13:22:28 +0100

    done with nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Header, Navbar } from "./Container";
 
 function App() {
   return (
-    <div>
+    <div className="bg-[#e0e7ff]">
       <div>
         <Navbar />
         <Header />
--- src/Container/Navbar.jsx
+++ src/Container/Navbar.jsx
@@ -2,9 +2,27 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div>
-      <div>
-        <p>navbar</p>
+    <div className="flex flex-wrap items-center justify-between px-4 py-2 ">
+      {/* Left Logo */}
+      <div className="flex items-center gap-2">
+        <i className="fa fa-ravelry fa-2x" aria-hidden="true"></i>
+        <p className="text-2xl font-semibold">JobHuntly</p>
+      </div>
+
+      {/* Middle Nav Links */}
+      <div className="hidden md:flex gap-4 text-slate-600  font-serif">
+        <p className="hover:text-blue-500 cursor-pointer">Find Jobs</p>
+        <p className="hover:text-blue-500 cursor-pointer">Browse Companies</p>
+      </div>
+
+      {/* Right Buttons */}
+      <div className="flex items-center gap-4 mt-2 md:mt-0">
+        <p className="text-blue-700 font-serif hover:bg-blue-500 hover:text-white px-3 py-1 rounded-md cursor-pointer">
+          Login
+        </p>
+        <button className="border w-[100px] h-[40px] rounded-[7px] text-white bg-blue-700 border-blue-700 transition-all duration-200 transform hover:scale-105 hover:shadow-md">
+          Sign up
+        </button>
       </div>
     </div>
   );
