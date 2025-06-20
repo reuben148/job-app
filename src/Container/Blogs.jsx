fab2d7df7f0afae232b9edbd7817536145dc555c
reuben148 <reubenluka555@gmail.com>
2025-06-20 11:43:26 +0100

    done with blogs

--- src/Container/Blogs.jsx
+++ src/Container/Blogs.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { Button } from "../Content";
 
 function Blogs() {
   return (
-    <div>
-      <div>
-        <p>blogs</p>
+    <div className="flex justify-center px-4 py-8">
+      <div className="w-full max-w-[950px] rounded-br-[50px] rounded-tl-[50px] bg-gradient-to-r from-[#3b82f6] to-[#8b5cf6] border-2 border-blue-500 p-6 md:flex md:justify-between md:items-center">
+        {/* Text Content */}
+        <div className="md:w-1/2">
+          <p className="text-4xl md:text-5xl font-mono text-white py-4 px-2 md:px-6">
+            Start Posting <br /> Jobs Today
+          </p>
+          <p className="text-white text-sm md:text-base px-2 md:px-6 pb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing <br />
+            elit.
+          </p>
+          <div className="px-2 md:px-6">
+            <Button />
+          </div>
+        </div>
+
+        {/* Image */}
+        <div className="md:w-1/2 flex justify-center md:justify-end mt-6 md:mt-0">
+          <img
+            src="dash.jpg"
+            alt="Dashboard"
+            className="w-[220px] md:w-[300px] rounded-md shadow-md"
+          />
+        </div>
       </div>
     </div>
   );
---
+++ src/Content/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Button() {
+  return (
+    <div>
+      <div>
+        <button className="px-6 ml-[35px] mt-[10px] py-3 rounded-lg border-2 border-purple-600 text-purple-600 bg-white font-semibold shadow-sm transition-all duration-300 hover:bg-purple-600 hover:text-white hover:shadow-lg hover:scale-105">
+          Sign up For Free
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Button;
